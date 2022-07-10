6987a4ff91a791ea66fb984dd0259d72569f2d24
Prez <simulo2000@gmail.com>
2022-07-10 13:20:00 +0200

    CQRS wip

---
+++ CQRS/Source/WebApi/Core/Enums/UserDepartment.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Core.Enums
+{
+    public enum UserDepartment
+    {
+        Finance = 1,
+        IT = 2,
+        Manager = 3
+    }
+}
---
+++ CQRS/Source/WebApi/Domain/Entities/User/User.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Core.Enums;
+
+namespace WebApi.Domain.Entities.User
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public UserDepartment Department { get; set; }
+    }
+}

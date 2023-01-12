da1aa4a973387d539e8551601e5943f0cf1e8188
Prez <simulo2000@gmail.com>
2023-01-13 00:30:30 +0100

    add some docker stuff

--- SqlServerDocker/restore-and-sanitize-backup.sql
+++ SqlServerDocker/restore-and-sanitize-backup.sql
@@ -1,15 +1,19 @@
+use master
+
 RESTORE DATABASE [AdventureWorks] FROM DISK = '/tmp/AdventureWorksLT2017.bak' WITH FILE = 1,
 MOVE 'AdventureWorksLT2012_Data' TO '/var/opt/mssql/data/AdventureWorks.mdf',
 MOVE 'AdventureWorksLT2012_Log' TO '/var/opt/mssql/data/AdventureWorks.ldf',
 NOUNLOAD, REPLACE, STATS = 5
 GO
 
+use AdventureWorks
+
 UPDATE SalesLT.Customer
 SET EmailAddress = FirstName + '@notreal.com',
-	LastName = 'Customer',
-	Phone = FORMAT(CAST(LEFT(CAST(ABS(CAST(CAST(NEWID() as BINARY(10)) as INT)) as VARCHAR(max)) + '0000000000', 10) as BIGINT), '###-###-####'),
-	PasswordHash = '',
-	PasswordSalt = '';
+LastName = 'Customer',
+Phone = FORMAT(CAST(LEFT(CAST(ABS(CAST(CAST(NEWID() as BINARY(10)) as INT)) as VARCHAR(max)) + '0000000000', 10) as BIGINT), '###-###-####'),
+PasswordHash = '',
+PasswordSalt = '';
 
 -- Use this to find out the files names used above (run management studio as admin)
 -- RESTORE FILELISTONLY FROM DISK = 'C:\Data\Arbeit\Development\professional-trainings\SqlServerDocker\AdventureWorksLT2017.bak'
--- SqlServerDocker/restore-backup.sql
+++ SqlServerDocker/restore-backup.sql
@@ -1,3 +1,5 @@
+use master
+
 RESTORE DATABASE [AdventureWorks] FROM DISK = '/tmp/AdventureWorksLT2017.bak' WITH FILE = 1,
 MOVE 'AdventureWorksLT2012_Data' TO '/var/opt/mssql/data/AdventureWorks.mdf',
 MOVE 'AdventureWorksLT2012_Log' TO '/var/opt/mssql/data/AdventureWorks.ldf',
